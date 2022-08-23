65f31e38a8f319d9fc2143bb9fb89d305b1c553d
Md Sumon Islam <sumon.99.sis@gmail.com>
2022-08-23 12:19:56 +0600

    main origin

---
+++ asset/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Software Engineer","Back End Developer","Web Desigener", "Freelancer"],
+        typeSpeed: 80,
+        backSpeed: 60,
+        loop: true
+    });
+});
--- html-css-static-website/asset/js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["Software Engineer","Back End Developer","Web Desigener", "Freelancer"],
-        typeSpeed: 80,
-        backSpeed: 60,
-        loop: true
-    });
-});
